5ba1bd3d5045362bfda57d67a59d6fc1bdd4f40c
Sara <sara.667@outlook.com>
2023-06-12 18:17:24 -0600

    .

--- main.js
+++ main.js
@@ -24,3 +24,22 @@ function linkAction() {
   navMenu.classList.toggle("show-menu");
 }
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/************* OPEN CLOSE SKILLS  ****************/
+const skillsContent = document.getElementsByClassName("skills__content");
+const skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = "skills__content skills__close";
+  }
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener("click", toggleSkills);
+});
