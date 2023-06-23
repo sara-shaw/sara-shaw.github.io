9288ea6fc48b789efc9c57858b1cfcc411db3920
Sara <sara.667@outlook.com>
2023-06-22 20:51:07 -0600

    swiper portfolio section

--- js/main.js
+++ js/main.js
@@ -65,3 +65,17 @@ tabs.forEach((tab) => {
     tab.classList.add("qualification__active");
   });
 });
+
+/******************* PORTFOLIO SWIPER *******************/
+let swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
